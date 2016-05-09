77915d6f06b4b6388550f176d60450cb8e686797
Luke Shingles <luke.shingles@gmail.com>
2016-05-09 14:20:41 +0100

    Refactor slice3dmodel.py

--- slice3dmodel/slice3dmodel.py
+++ slice3dmodel/slice3dmodel.py
@@ -6,24 +6,25 @@ import sys
 
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    description='Covert abundances.txt and model.txt from 3D to a '
-                'one dimensional slice.')
-parser.add_argument('-inputfolder', action='store', default='3dmodel',
-                    help='Path to folder with 3D files')
-parser.add_argument('-axis', action='store', dest='chosenaxis', default='x',
-                    choices=['x', 'y', 'z'],
-                    help='Slice axis (x, y, or z)')
-parser.add_argument('-outputfolder', action='store', default='1dslice',
-                    help='Path to folder in which to store 1D output files')
-parser.add_argument('-opdf', action='store', dest='pdfoutputfile',
-                    default='plotmodel.pdf',
-                    help='Path/filename for PDF plot.')
-args = parser.parse_args()
-
 
 def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description='Covert abundances.txt and model.txt from 3D to a '
+                    'one dimensional slice.')
+    parser.add_argument('-inputfolder', action='store', default='3dmodel',
+                        help='Path to folder with 3D files')
+    parser.add_argument('-axis', action='store', dest='chosenaxis',
+                        default='x', choices=['x', 'y', 'z'],
+                        help='Slice axis (x, y, or z)')
+    parser.add_argument('-outputfolder', action='store', default='1dslice',
+                        help='Path to folder in which to store 1D output'
+                        ' files')
+    parser.add_argument('-opdf', action='store', dest='pdfoutputfile',
+                        default='plotmodel.pdf',
+                        help='Path/filename for PDF plot.')
+    args = parser.parse_args()
+
     xlist = []
     ylist = []
 
@@ -36,59 +37,47 @@ def main():
         fmodelin.readline()  # v_max in [cm/s]
 
         while True:
-            blockline1 = fmodelin.readline()
-            blockline2 = fmodelin.readline()
+            # two lines making up a model grid cell
+            block = fmodelin.readline(), fmodelin.readline()
 
-            if not blockline1 or not blockline2:
+            if not block[0] or not block[1]:
                 break
 
-            line1split = blockline1.split()
-
-            if len(line1split) == 5:
-                (cellid, posx, posy, posz, rho) = line1split
+            cell = {}
+            blocksplit = block[0].split(), block[1].split()
+            if len(blocksplit[0]) == 5:
+                (cell['cellid'], cell['posx'], cell['posy'], cell['posz'],
+                 cell['rho']) = blocksplit[0]
             else:
                 print("Wrong line size")
                 sys.exit()
 
-            line2split = blockline2.split()
-
-            if len(line2split) == 5:
-                (ffe, f56ni, fco, f52fe, f48cr) = map(float, line2split)
+            if len(blocksplit[1]) == 5:
+                (cell['ffe'], cell['f56ni'], cell['fco'], cell['f52fe'],
+                 cell['f48cr']) = map(float, blocksplit[1])
             else:
                 print("Wrong line size")
                 sys.exit()
 
-            if posx != "0.0000000" and (
-                    args.chosenaxis != 'x' or float(posx) < 0.):
-                keep_cell = False
-            elif posy != "0.0000000" and (
-                    args.chosenaxis != 'y' or float(posy) < 0.):
-                keep_cell = False
-            elif posz != "0.0000000" and (
-                    args.chosenaxis != 'z' or float(posz) < 0.):
-                keep_cell = False
+            if cell['posx'] != "0.0000000" and (
+                    args.chosenaxis != 'x' or float(cell['posx']) < 0.):
+                pass
+            elif cell['posy'] != "0.0000000" and (
+                    args.chosenaxis != 'y' or float(cell['posy']) < 0.):
+                pass
+            elif cell['posz'] != "0.0000000" and (
+                    args.chosenaxis != 'z' or float(cell['posz']) < 0.):
+                pass
             else:
-                keep_cell = True
-
-            if keep_cell:
                 outcellid += 1
-                dict3dcellidto1dcellid[int(cellid)] = outcellid
-                dist = math.sqrt(
-                    float(posx) ** 2 + float(posy) ** 2 + float(posz) ** 2)
-                velocity = float(dist) / float(t_model) / 86400. / 1.e5
-                listout.append(
-                    '{0:6d}  {1:8.2f}  {2:8.5f}  {3:.5f}  '
-                    '{4:.5f}  {5:.5f}  {6:.5f}  {7:.5f}'
-                    .format(outcellid, velocity,
-                            math.log10(max(float(rho), 1e-100)),
-                            ffe, f56ni, fco, f52fe, f48cr))
+                dict3dcellidto1dcellid[int(cell['cellid'])] = outcellid
+                append_cell_to_output(cell, outcellid, t_model, listout,
+                                      xlist, ylist)
                 print("Cell {0:4d} input1: {1}".format(
-                    outcellid, blockline1.rstrip()))
+                    outcellid, block[0].rstrip()))
                 print("Cell {0:4d} input2: {1}".format(
-                    outcellid, blockline2.rstrip()))
+                    outcellid, block[1].rstrip()))
                 print("Cell {0:4d} output: {1}".format(outcellid, listout[-1]))
-                xlist.append(velocity)
-                ylist.append(f56ni)
 
     with open(os.path.join(args.outputfolder, 'model.txt'), 'w') as fmodelout:
         fmodelout.write("{0:7d}\n".format(outcellid))
@@ -96,15 +85,16 @@ def main():
         for line in listout:
             fmodelout.write(line + "\n")
 
-    convert_abundance_file(dict3dcellidto1dcellid)
+    convert_abundance_file(args.inputfolder, args.outputfolder,
+                           dict3dcellidto1dcellid)
 
-    make_plot(xlist, ylist)
+    make_plot(xlist, ylist, args.pdfoutputfile)
 
 
-def convert_abundance_file(dict3dcellidto1dcellid):
+def convert_abundance_file(inputfolder, outputfolder, dict3dcellidto1dcellid):
     with open(os.path.join(
-        args.inputfolder, 'abundances.txt'), 'r') as fabundancesin, open(
-            os.path.join(args.outputfolder, 'abundances.txt'),
+        inputfolder, 'abundances.txt'), 'r') as fabundancesin, open(
+            os.path.join(outputfolder, 'abundances.txt'),
             'w') as fabundancesout:
         currentblock = []
         keepcurrentblock = False
@@ -130,15 +120,33 @@ def convert_abundance_file(dict3dcellidto1dcellid):
         print("WARNING: unfinished block")
 
 
-def make_plot(xlist, ylist):
+def make_plot(xlist, ylist, pdfoutputfile):
     fig, ax = plt.subplots(1, 1, sharey=True, figsize=(6, 4),
                            tight_layout={
                                "pad": 0.2, "w_pad": 0.0, "h_pad": 0.0})
     ax.set_xlabel(r'v (km/s)')
     ax.set_ylabel(r'Density (g/cm$^3$)')
     ax.plot(xlist, ylist, lw=1.5)
-    fig.savefig(args.pdfoutputfile, format='pdf')
+    fig.savefig(pdfoutputfile, format='pdf')
     plt.close()
+
+
+def append_cell_to_output(cell, outcellid, t_model, listout, xlist, ylist):
+    dist = math.sqrt(
+        float(cell['posx']) ** 2 + float(cell['posy']) ** 2 +
+        float(cell['posz']) ** 2)
+    velocity = float(dist) / float(t_model) / 86400. / 1.e5
+
+    listout.append(
+        '{0:6d}  {1:8.2f}  {2:8.5f}  {3:.5f}  '
+        '{4:.5f}  {5:.5f}  {6:.5f}  {7:.5f}'
+        .format(outcellid, velocity,
+                math.log10(max(float(cell['rho']), 1e-100)),
+                cell['ffe'], cell['f56ni'], cell['fco'],
+                cell['f52fe'], cell['f48cr']))
+
+    xlist.append(velocity)
+    ylist.append(cell['f56ni'])
 
 
 if __name__ == "__main__":
