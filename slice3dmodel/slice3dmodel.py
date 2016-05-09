22ba628792f56bc00d90ea544b86d4b07039c2d7
Luke Shingles <luke.shingles@gmail.com>
2016-05-09 13:44:36 +0100

    Refactor slice3dmodel, add argparse support

--- slice3dmodel/slice3dmodel.py
+++ slice3dmodel/slice3dmodel.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python3
-import sys
+import argparse
 import math
-import matplotlib
+import os
+import sys
+
 import matplotlib.pyplot as plt
-matplotlib.use('PDF')
+
+parser = argparse.ArgumentParser(
+    description='Covert abundances.txt and model.txt from 3D to a '
+                'one dimensional slice.')
+parser.add_argument('-inputfolder', action='store', default='3dmodel',
+                    help='Path to folder with 3D files')
+parser.add_argument('-axis', action='store', dest='chosenaxis', default='x',
+                    help='Slice axis (x, y, or z)')
+parser.add_argument('-outputfolder', action='store', default='1dslice',
+                    help='Path to folder in which to store 1D output files')
+parser.add_argument('-opdf', action='store', dest='pdfoutputfile',
+                    default='plotmodel.pdf',
+                    help='path/filename for PDF file')
+args = parser.parse_args()
 
 
 def main():
-    inputfolder = '3dmodel/'
-    outputfolder = '1dslice/'
-
-    fig, ax = plt.subplots(1, 1, sharey=True, figsize=(6, 4), tight_layout={
-                           "pad": 0.2, "w_pad": 0.0, "h_pad": 0.0})
     xlist = []
     ylist = []
-
-    chosenaxis = 'x'
 
     listout = []
     dict3dcellidto1dcellid = {}
     outcellid = 0
-    with open(inputfolder + 'model.txt', 'r') as fmodelin:
-        npts_model3d = fmodelin.readline()
+    with open(os.path.join(args.inputfolder, 'model.txt'), 'r') as fmodelin:
+        fmodelin.readline()  # npts_model3d
         t_model = fmodelin.readline()  # days
-        vmax = fmodelin.readline()  # cm/s
+        fmodelin.readline()  # v_max in [cm/s]
 
         while True:
             blockline1 = fmodelin.readline()
@@ -48,34 +56,54 @@ def main():
                 print("Wrong line size")
                 sys.exit()
 
-            if ((posx == "0.0000000" or (chosenaxis == 'x' and float(posx) >= 0.)) and
-                    (posy == "0.0000000" or (chosenaxis == 'y' and float(posy) >= 0.)) and
-                    (posz == "0.0000000" or (chosenaxis == 'z' and float(posz) >= 0.))):
+            if posx != "0.0000000" and (
+                    args.chosenaxis != 'x' or float(posx) < 0.):
+                keep_cell = False
+            elif posy != "0.0000000" and (
+                    args.chosenaxis != 'y' or float(posy) < 0.):
+                keep_cell = False
+            elif posz != "0.0000000" and (
+                    args.chosenaxis != 'z' or float(posz) < 0.):
+                keep_cell = False
+            else:
+                keep_cell = True
+
+            if keep_cell:
                 outcellid += 1
                 dict3dcellidto1dcellid[int(cellid)] = outcellid
-                dist = math.sqrt(float(posx) ** 2 + float(posy) ** 2 + float(posz) ** 2)
+                dist = math.sqrt(
+                    float(posx) ** 2 + float(posy) ** 2 + float(posz) ** 2)
                 velocity = float(dist) / float(t_model) / 86400. / 1.e5
                 listout.append(
-                    "{0:6d}  {1:8.2f}  {2:8.5f}  {3:.5f}  {4:.5f}  {5:.5f}  {6:.5f}  {7:.5f}"
-                    .format(outcellid, velocity, math.log10(max(float(rho), 1e-100)),
+                    '{0:6d}  {1:8.2f}  {2:8.5f}  {3:.5f}  '
+                    '{4:.5f}  {5:.5f}  {6:.5f}  {7:.5f}'
+                    .format(outcellid, velocity,
+                            math.log10(max(float(rho), 1e-100)),
                             ffe, f56ni, fco, f52fe, f48cr))
-                print("Cell {0:4d} input1: {1}".format(outcellid, blockline1.rstrip()))
-                print("Cell {0:4d} input2: {1}".format(outcellid, blockline2.rstrip()))
+                print("Cell {0:4d} input1: {1}".format(
+                    outcellid, blockline1.rstrip()))
+                print("Cell {0:4d} input2: {1}".format(
+                    outcellid, blockline2.rstrip()))
                 print("Cell {0:4d} output: {1}".format(outcellid, listout[-1]))
                 xlist.append(velocity)
                 ylist.append(f56ni)
 
-    ax.set_xlabel(r'v (km/s)')
-    ax.set_ylabel(r'Density (g/cm$^3$)')
-
-    with open(outputfolder + 'model.txt', 'w') as fmodelout:
+    with open(os.path.join(args.outputfolder, 'model.txt'), 'w') as fmodelout:
         fmodelout.write("{0:7d}\n".format(outcellid))
         fmodelout.write(t_model)
         for line in listout:
             fmodelout.write(line + "\n")
 
-    with open(inputfolder + 'abundances.txt', 'r') as fabundancesin, open(
-              outputfolder + 'abundances.txt', 'w') as fabundancesout:
+    convert_abundance_file(dict3dcellidto1dcellid)
+
+    make_plot(xlist, ylist)
+
+
+def convert_abundance_file(dict3dcellidto1dcellid):
+    with open(os.path.join(
+        args.inputfolder, 'abundances.txt'), 'r') as fabundancesin, open(
+            os.path.join(args.outputfolder, 'abundances.txt'),
+            'w') as fabundancesout:
         currentblock = []
         keepcurrentblock = False
         for line in fabundancesin:
@@ -99,9 +127,17 @@ def main():
     if keepcurrentblock:
         print("WARNING: unfinished block")
 
+
+def make_plot(xlist, ylist):
+    fig, ax = plt.subplots(1, 1, sharey=True, figsize=(6, 4),
+                           tight_layout={
+                               "pad": 0.2, "w_pad": 0.0, "h_pad": 0.0})
+    ax.set_xlabel(r'v (km/s)')
+    ax.set_ylabel(r'Density (g/cm$^3$)')
     ax.plot(xlist, ylist, lw=1.5)
-    fig.savefig('plotmodel.pdf', format='pdf')
+    fig.savefig(args.pdfoutputfile, format='pdf')
     plt.close()
+
 
 if __name__ == "__main__":
     main()
