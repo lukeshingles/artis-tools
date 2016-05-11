da9bb395260e7a817ed31767d3154fb7fa3845a9
Luke Shingles <luke.shingles@gmail.com>
2016-05-11 01:56:37 +0100

    fix duplicate light curves, use DataFrame.plot, rename ax to axis

--- plotartislightcurve.py
+++ plotartislightcurve.py
@@ -6,8 +6,7 @@ import pandas as pd
 
 
 def main():
-    lcfiles = glob.glob('light_curve.out') + glob.glob(
-        '**/light_curve.out', recursive=True)
+    lcfiles = glob.glob('**/light_curve.out', recursive=True)
 
     if not lcfiles:
         print('No light_curve.out files found.')
@@ -17,29 +16,29 @@ def main():
 
 
 def makeplot(lcfiles):
-    fig, ax = plt.subplots(1, 1, sharey=True, figsize=(8, 5), tight_layout={
+    fig, axis = plt.subplots(1, 1, sharey=True, figsize=(8, 5), tight_layout={
                            "pad": 0.2, "w_pad": 0.0, "h_pad": 0.0})
 
     for n, lcfilename in enumerate(lcfiles):
+        print(lcfilename)
         lightcurvedata = pd.read_csv(lcfilename, delim_whitespace=True,
-                                     header=None)
+                                     header=None, names=['time', 'flux',
+                                                         'other'])
 
         linelabel = '{0}'.format(lcfilename.split('/light_curve.out')[0])
 
         linestyle = ['-', '--'][int(n / 7)]
 
-        arraytime, arrayflux = zip(*lightcurvedata.iloc[:, :2].values)
+        lightcurvedata.plot(x='time', y='flux', lw=1.5, ax=axis,
+                            linestyle=linestyle, label=linelabel)
 
-        ax.plot(arraytime, arrayflux, lw=1.5, linestyle=linestyle,
-                label=linelabel)
+    # axis.set_xlim(xmin=xminvalue,xmax=xmaxvalue)
+    # axis.set_ylim(ymin=-0.1,ymax=1.3)
 
-    # ax.set_xlim(xmin=xminvalue,xmax=xmaxvalue)
-    # ax.set_ylim(ymin=-0.1,ymax=1.3)
-
-    ax.legend(loc='best', handlelength=2, frameon=False, numpoints=1,
-              prop={'size': 9})
-    ax.set_xlabel(r'Time (days)')
-    ax.set_ylabel(r'$\propto$ Flux')
+    axis.legend(loc='best', handlelength=2, frameon=False, numpoints=1,
+                prop={'size': 9})
+    axis.set_xlabel(r'Time (days)')
+    axis.set_ylabel(r'$\propto$ Flux')
 
     fig.savefig('plotlightcurve.pdf', format='pdf')
     plt.close()
--- plotartisradfield.py
+++ plotartisradfield.py
@@ -62,31 +62,31 @@ def draw_plot(radfielddata, args):
     """
         Draw the bin edges, fitted field, and emergent spectrum
     """
-    fig, ax = plt.subplots(1, 1, sharex=True, figsize=(8, 4),
-                           tight_layout={
-                               "pad": 0.2, "w_pad": 0.0, "h_pad": 0.0})
+    fig, axis = plt.subplots(1, 1, sharex=True, figsize=(8, 4),
+                             tight_layout={
+                                 "pad": 0.2, "w_pad": 0.0, "h_pad": 0.0})
 
     binedges = [C / radfielddata['nu_lower'].iloc[0] * 1e10] + \
         list(C / radfielddata['nu_upper'] * 1e10)
 
-    ymax = plot_field_estimators(ax, radfielddata)
-    ax.vlines(binedges, ymin=0.0, ymax=ymax, linewidth=0.2,
-              color='red', label='')
-    plot_fitted_field(ax, radfielddata)
-    plot_specout(ax, ymax)
+    ymax = plot_field_estimators(axis, radfielddata)
+    axis.vlines(binedges, ymin=0.0, ymax=ymax, linewidth=1.0,
+                color='red', label='')
+    plot_fitted_field(axis, radfielddata)
+    plot_specout(axis, ymax)
 
-    ax.set_xlabel(r'Wavelength ($\AA$)')
-    ax.set_ylabel(r'J$_\lambda$ [erg/cm$^2$/m]')
-    ax.set_xlim(xmin=args.xmin, xmax=args.xmax)
+    axis.set_xlabel(r'Wavelength ($\AA$)')
+    axis.set_ylabel(r'J$_\lambda$ [erg/cm$^2$/m]')
+    axis.set_xlim(xmin=args.xmin, xmax=args.xmax)
 
-    ax.legend(loc='best', handlelength=2,
-              frameon=False, numpoints=1, prop={'size': 13})
+    axis.legend(loc='best', handlelength=2,
+                frameon=False, numpoints=1, prop={'size': 13})
 
     fig.savefig('plotradfield.pdf', format='pdf')
     plt.close()
 
 
-def plot_field_estimators(ax, radfielddata):
+def plot_field_estimators(axis, radfielddata):
     """
         Plot the dJ/dlambda estimators for each bin
     """
@@ -99,12 +99,12 @@ def plot_field_estimators(ax, radfielddata):
             (C / row['nu_upper'])
         yvalues.append(row['J'] / dlambda)
         yvalues.append(row['J'] / dlambda)
-    ax.plot(xvalues, yvalues, linewidth=1, label='Field estimators',
+    axis.plot(xvalues, yvalues, linewidth=1, label='Field estimators',
             color='blue')
     return max(yvalues)
 
 
-def plot_fitted_field(ax, radfielddata):
+def plot_fitted_field(axis, radfielddata):
     """
         Plot the fitted diluted blackbody of each bins
     """
@@ -122,17 +122,17 @@ def plot_fitted_field(ax, radfielddata):
             fittedxvalues.append(C / nu * 1e10)
             fittedyvalues.append(j_lambda)
 
-    ax.plot(fittedxvalues, fittedyvalues, linewidth=1, color='green',
+    axis.plot(fittedxvalues, fittedyvalues, linewidth=1, color='green',
             label='Fitted field')
 
 
-def plot_specout(ax, peak_value):
+def plot_specout(axis, peak_value):
     spectrum = af.get_spectrum('../example_run_testing/spec.out',
                                10, 10, normalised=True)
     spectrum['f_lambda'] = spectrum['f_lambda'] * peak_value
 
     spectrum.plot(x='lambda_angstroms',
-                  y='f_lambda', ax=ax,
+                  y='f_lambda', ax=axis,
                   linewidth=1, color='black',
                   label='Emergent spectrum')
 
