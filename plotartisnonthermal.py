ff6cc2876ebf10f70ed5f3eb4a2f40df92975533
Luke Shingles <luke.shingles@gmail.com>
2016-10-26 01:42:44 +0100

    Fix line continuation character

--- plotartisnonthermal.py
+++ plotartisnonthermal.py
@@ -45,7 +45,7 @@ def main():
         af.showtimesteptimes('spec.out')
     else:
         nonthermaldata = None
-        nonthermal_files = glob.glob('nonthermalspec_????.out', recursive=True) + _
+        nonthermal_files = glob.glob('nonthermalspec_????.out', recursive=True) + \
             glob.glob('nonthermalspec-????.out', recursive=True) + glob.glob('nonthermalspec.out', recursive=True)
         for nonthermal_file in nonthermal_files:
             print('Loading {:}...'.format(nonthermal_file))
--- plotartisradfield.py
+++ plotartisradfield.py
@@ -47,7 +47,7 @@ def main():
         af.showtimesteptimes('spec.out')
     else:
         radfielddata = None
-        radfield_files = glob.glob('radfield_????.out', recursive=True) + _
+        radfield_files = glob.glob('radfield_????.out', recursive=True) + \
             glob.glob('radfield-????.out', recursive=True) + glob.glob('radfield.out', recursive=True)
         for radfield_file in radfield_files:
             print('Loading {:}...'.format(radfield_file))
