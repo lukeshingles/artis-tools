e8b589c1b51b94d183133b9298bb2a2bbe6320db
Luke Shingles <luke.shingles@gmail.com>
2017-03-27 16:07:16 +0100

    Fix calculated delta_t for final timestep

--- readartisfiles.py
+++ readartisfiles.py
@@ -131,8 +131,14 @@ def get_spectrum(specfilename, timesteplow, timestephigh=-1, fnufilterfunc=None)
 
     arraynu = specdata['0']
 
-    delta_t = (float(get_timestep_time(specfilename, timesteplow + 1)) -
-               float(get_timestep_time(specfilename, timesteplow)))
+    timestep_final = len(specdata.columns) - 2  # -1 to exclude 0 column and -1 since we start counting at zero
+    if timesteplow + 1 < timestep_final:
+        delta_t = (float(get_timestep_time(specfilename, timesteplow + 1)) -
+                   float(get_timestep_time(specfilename, timesteplow)))
+    else:
+        delta_t = (float(get_timestep_time(specfilename, timesteplow)) -
+                   float(get_timestep_time(specfilename, timesteplow - 1)))
+
     delta_t_alltimesteps = delta_t
     array_fnu = specdata[specdata.columns[timesteplow + 1]] * delta_t
 
