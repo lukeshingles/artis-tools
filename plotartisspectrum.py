Fix normalisation of emission plot

plotartisspectrum.py
@@ -82,8 +82,7 @@ def get_flux_contributions(emissionfilename, absorptionfilename, elementlist, ma
     emissiondata = np.loadtxt(emissionfilename)
     print(f"Reading {absorptionfilename}")
     absorptiondata = np.loadtxt(absorptionfilename)
-    c = const.c.to('m/s').value
-    arraylambda = c / arraynu
+    arraylambda = const.c.to('angstrom/s').value / arraynu
 
     nelements = len(elementlist)
     maxyvalueglobal = 0.0
@@ -103,34 +102,42 @@ def get_flux_contributions(emissionfilename, absorptionfilename, elementlist, ma
             ionserieslist.append((nelements * maxion + element * maxion + ion, 'bound-free'))
 
             for (selectedcolumn, emissiontype) in ionserieslist:
-                array_fnu_emission = emissiondata[timestepmin::len(timearray), selectedcolumn]
+                delta_t_sum = 0
+                array_fnu_emission = np.zeros(len(emissiondata[timestepmin::len(timearray), selectedcolumn]))
 
-                for timeindex in range(timestepmin + 1, timestepmax + 1):
-                    array_fnu_emission += emissiondata[timeindex::len(timearray), selectedcolumn]
+                for timestep in range(timestepmin, timestepmax + 1):
+                    delta_t = af.get_timestep_time_delta(timestep, timearray)
+                    delta_t_sum += delta_t
+                    array_fnu_emission += emissiondata[timestep::len(timearray), selectedcolumn] * delta_t
+
+                array_fnu_emission /= delta_t_sum
 
                 if selectedcolumn < nelements * maxion:
-                    array_fnu_absorption = absorptiondata[timestepmin::len(timearray), selectedcolumn]
+                    delta_t_sum = 0
+                    array_fnu_absorption = np.zeros(len(absorptiondata[timestepmin::len(timearray), selectedcolumn]))
 
-                    for timeindex in range(timestepmin + 1, timestepmax + 1):
-                        array_fnu_absorption += absorptiondata[timeindex::len(timearray), selectedcolumn]
+                    for timestep in range(timestepmin, timestepmax + 1):
+                        delta_t = af.get_timestep_time_delta(timestep, timearray)
+                        delta_t_sum += delta_t
+                        array_fnu_absorption += absorptiondata[timestep::len(timearray), selectedcolumn] * delta_t
 
-                # rough normalisation for stacked timesteps. replace with dividing by time
-                array_fnu_emission = array_fnu_emission / (timestepmax - timestepmin + 1)
+                    array_fnu_absorption /= delta_t_sum
 
                 # best to use the filter on this list (because it hopefully has
                 # regular sampling)
-                array_fnu_emission = scipy.signal.savgol_filter(array_fnu_emission, 5, 2)
-                array_flambda_emission = array_fnu_emission * (arraynu ** 2) / c
+                # array_fnu_emission = scipy.signal.savgol_filter(array_fnu_emission, 5, 2)
+                array_flambda_emission = array_fnu_emission * arraynu / arraylambda
 
                 if selectedcolumn <= nelements * maxion:
                     array_fnu_absorption = array_fnu_absorption / (timestepmax - timestepmin + 1)
                     array_fnu_absorption = scipy.signal.savgol_filter(array_fnu_absorption, 5, 2)
-                    array_flambda_absorption = array_fnu_absorption * (arraynu ** 2) / c
+
+                    array_flambda_absorption = array_fnu_absorption * arraynu / arraylambda
                 else:
                     array_flambda_absorption = np.zeros(len(array_fnu_emission))
 
                 maxyvaluethisseries = max(
-                    [array_flambda_emission[i] if (args.xmin < (1e10 * arraylambda[i]) < args.xmax) else -99.0
+                    [array_flambda_emission[i] if (args.xmin < arraylambda[i] < args.xmax) else -99.0
                      for i in range(len(array_flambda_emission))])
 
                 maxyvalueglobal = max(maxyvalueglobal, maxyvaluethisseries)

readartisfiles.py
@@ -131,24 +131,16 @@ def get_spectrum(specfilename, timesteplow, timestephigh=-1, fnufilterfunc=None)
 
     arraynu = specdata['0']
 
-    timestep_final = len(specdata.columns) - 2  # -1 to exclude 0 column and -1 since we start counting at zero
-    if timesteplow + 1 < timestep_final:
-        delta_t = (float(get_timestep_time(specfilename, timesteplow + 1)) -
-                   float(get_timestep_time(specfilename, timesteplow)))
-    else:
-        delta_t = (float(get_timestep_time(specfilename, timesteplow)) -
-                   float(get_timestep_time(specfilename, timesteplow - 1)))
+    delta_t_sum = 0
+    array_fnu = np.zeros(len(specdata))
+    timearray = specdata.columns[1:]
 
-    delta_t_alltimesteps = delta_t
-    array_fnu = specdata[specdata.columns[timesteplow + 1]] * delta_t
-
-    for timestep in range(timesteplow + 1, timestephigh + 1):
-        delta_t = (float(get_timestep_time(specfilename, timestep + 1)) -
-                   float(get_timestep_time(specfilename, timestep)))
-        delta_t_alltimesteps += delta_t
+    for timestep in range(timesteplow, timestephigh + 1):
+        delta_t = get_timestep_time_delta(timestep, timearray)
+        delta_t_sum += delta_t
         array_fnu += specdata[specdata.columns[timestep + 1]] * delta_t
 
-    array_fnu = array_fnu / delta_t_alltimesteps
+    array_fnu = array_fnu / delta_t_sum
 
     # best to use the filter on this list because it
     # has regular sampling
@@ -227,6 +219,19 @@ def get_timestep_time(specfilename, timestep):
         return get_timestep_times(specfilename)[timestep]
     else:
         return -1
+
+
+def get_timestep_time_delta(timestep, timearray):
+    """
+        Return the time in days between timestep and timestep + 1
+    """
+    timestep_final = len(timearray) - 1  # -1 since we start counting at zero
+    if timestep + 1 < timestep_final:
+        delta_t = (float(timearray[timestep + 1]) - float(timearray[timestep]))
+    else:
+        delta_t = (float(timearray[timestep]) - float(timearray[timestep]))
+
+    return delta_t
 
 
 def get_levels(adatafilename):