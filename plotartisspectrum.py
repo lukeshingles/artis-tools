dd54ace8cf11988b235c3757311d503e9afd5c35
Luke Shingles <luke.shingles@gmail.com>
2017-03-27 17:56:00 +0100

    Improve code readability

--- plotartisspectrum.py
+++ plotartisspectrum.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 import argparse
 import glob
-import math
-import numpy as np
 import os
 import sys
 import warnings
-
-import scipy.signal
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-from astropy import constants as const
 from collections import namedtuple
+
+import matplotlib.pyplot as plt
+# import matplotlib.ticker as ticker
+import numpy as np
+import scipy.signal
+from astropy import constants as const
 
 import readartisfiles as af
 
@@ -19,6 +18,8 @@ warnings.filterwarnings(action="ignore", module="scipy", message="^internal gels
 
 fluxcontributiontuple = namedtuple(
     'fluxcontribution', 'maxyvalue linelabel array_flambda_emission array_flambda_absorption')
+
+# TODO: input one or more ARTIS folders instead of file names
 
 
 def main():
@@ -149,6 +150,68 @@ def get_flux_contributions(emissionfilename, absorptionfilename, elementlist, ma
     return contribution_list, maxyvalueglobal
 
 
+def get_model_name_times(filename, specfilename, args):
+    timestepmin, timestepmax = af.get_minmax_timesteps(specfilename, args)
+    modelname = af.get_model_name(filename)
+
+    time_days_lower = float(af.get_timestep_time(specfilename, timestepmin))
+    time_days_upper = float(af.get_timestep_time(specfilename, timestepmax))
+
+    print(f'Plotting {modelname} ({filename}) timesteps {timestepmin} to {timestepmax} '
+          f'(t={time_days_lower}d to {time_days_upper}d)')
+
+    return modelname, timestepmin, timestepmax, time_days_lower, time_days_upper
+
+
+def plot_artis_spectra(axis, inputfiles, args):
+    """
+        Plot ARTIS emergent spectra
+    """
+
+    # dashesList = [(), (1.5, 2, 9, 2), (5, 1), (0.5, 2), (4, 2)]
+    # dash_capstyleList = ['butt', 'butt', 'butt', 'round', 'butt']
+    # colorlist = [(0, .8*158./255, 0.6*115./255), (204./255, 121./255, 167./255), (213./255, 94./255, 0.0)]
+    # inputfiles.sort(key=lambda x: os.path.dirname(x))
+    for index, filename in enumerate(inputfiles):
+        from_packets = os.path.basename(filename).startswith('packets')
+
+        if from_packets:
+            specfilename = os.path.join(os.path.dirname(filename), 'spec.out')
+        else:
+            specfilename = filename
+
+        (modelname, timestepmin, timestepmax,
+         time_days_lower, time_days_upper) = get_model_name_times(filename, specfilename, args)
+
+        linelabel = f'{modelname} at t={time_days_lower:.2f}d to {time_days_upper:.2f}d'
+
+        if from_packets:
+            # find any other packets files in the same directory
+            packetsfiles_thismodel = glob.glob(os.path.join(os.path.dirname(filename), 'packets**.out'))
+            print(packetsfiles_thismodel)
+            spectrum = af.get_spectrum_from_packets(packetsfiles_thismodel, time_days_lower, time_days_upper,
+                                                    lambda_min=args.xmin, lambda_max=args.xmax)
+        else:
+            # def filterfunc(arrayfnu):
+            #     from scipy.signal import savgol_filter
+            #     return savgol_filter(arrayfnu, 5, 2)
+
+            spectrum = af.get_spectrum(specfilename, timestepmin, timestepmax,)
+            #                          fnufilterfunc=filterfunc)
+
+        maxyvaluethisseries = spectrum.query(
+            '@args.xmin < lambda_angstroms and '
+            'lambda_angstroms < @args.xmax')['f_lambda'].max()
+
+        linestyle = ['-', '--'][int(index / 7) % 2]
+        spectrum['f_lambda_scaled'] = (spectrum['f_lambda'] / maxyvaluethisseries)
+        ycolumnname = 'f_lambda_scaled' if args.normalised else 'f_lambda'
+        spectrum.plot(x='lambda_angstroms', y=ycolumnname, ax=axis,
+                      linestyle=linestyle, linewidth=2.5 - (0.2 * index),
+                      label=linelabel, alpha=0.95, color=None)  # colorlist[index % len(colorlist)]
+        # dashes=dashesList[index], dash_capstyle=dash_capstyleList[index])
+
+
 def make_emission_plot(emissionfilename, axis, args):
     elementlist = af.get_composition_data(os.path.join(os.path.dirname(emissionfilename), 'compositiondata.txt'))
 
@@ -158,20 +221,8 @@ def make_emission_plot(emissionfilename, axis, args):
     specfilename = os.path.join(os.path.dirname(emissionfilename), 'spec.out')
     specdata = np.loadtxt(specfilename)
 
-    modelname = af.get_model_name(emissionfilename)
-
-    timestepmin, timestepmax = af.get_minmax_timesteps(specfilename, args)
-
-    time_in_days_lower = math.floor(float(af.get_timestep_time(specfilename, timestepmin)))
-    plotlabel = f'{modelname}\nt={time_in_days_lower}d'
-
-    if timestepmax > timestepmin:
-        time_in_days_upper = math.floor(float(af.get_timestep_time(specfilename, timestepmax)))
-        plotlabel += f' to {time_in_days_upper}d'
-        print(f'Plotting {modelname} timesteps {timestepmin} to {timestepmax} (t={time_in_days_lower}d'
-              f' to {time_in_days_upper}d)')
-    else:
-        print(f'Plotting {modelname} timestep {timestepmin} (t={time_in_days_lower}d)')
+    (modelname, timestepmin, timestepmax,
+     time_days_lower, time_days_upper) = get_model_name_times(emissionfilename, specfilename, args)
 
     timearray = specdata[0, 1:]
     arraynu = specdata[1:, 0]
@@ -199,10 +250,11 @@ def make_emission_plot(emissionfilename, axis, args):
     plotobjectlabels = list([x.linelabel for x in contribution_list])
 
     af.plot_reference_spectra(axis, plotobjects, plotobjectlabels, args, flambdafilterfunc=None,
-                              scale_to_peak=(maxyvalueglobal if args.normalised else None), lw=0.5)
+                              scale_to_peak=(maxyvalueglobal if args.normalised else None), linewidth=0.5)
 
-    axis.axhline(color='white', lw=1.0)
+    axis.axhline(color='white', linewidth=1.0)
 
+    plotlabel = f'{modelname}\nt={time_days_lower:.2f}d to {time_days_upper:.2f}d'
     axis.annotate(plotlabel, xy=(0.05, 0.96), xycoords='axes fraction',
                   horizontalalignment='left', verticalalignment='top', fontsize=8)
 
@@ -241,7 +293,6 @@ def make_spectrum_plot(inputfiles, axis, args):
         axis.set_ylabel(r'F$_\lambda$ at 1 Mpc [erg/s/cm$^2$/$\AA$]')
 
 
-
 def make_plot(inputfiles, args):
     import matplotlib.ticker as ticker
 
@@ -265,65 +316,6 @@ def make_plot(inputfiles, args):
     fig.savefig(filenameout, format='pdf')
     print(f'Saved {filenameout}')
     plt.close()
-
-
-def plot_artis_spectra(axis, inputfiles, args):
-    """
-        Plot ARTIS emergent spectra
-    """
-
-    # dashesList = [(), (1.5, 2, 9, 2), (5, 1), (0.5, 2), (4, 2)]
-    # dash_capstyleList = ['butt', 'butt', 'butt', 'round', 'butt']
-    # colorlist = [(0, .8*158./255, 0.6*115./255), (204./255, 121./255, 167./255), (213./255, 94./255, 0.0)]
-    # inputfiles.sort(key=lambda x: os.path.dirname(x))
-    for index, filename in enumerate(inputfiles):
-        modelname = af.get_model_name(filename)
-
-        from_packets = os.path.basename(filename).startswith('packets')
-
-        if from_packets:
-            specfilename = os.path.join(os.path.dirname(filename), 'spec.out')
-        else:
-            specfilename = filename
-
-        timestepmin, timestepmax = af.get_minmax_timesteps(specfilename, args)
-
-        time_days_lower = float(af.get_timestep_time(specfilename, timestepmin))
-        linelabel = f'{modelname} at t={time_days_lower:.2f}d'
-
-        if timestepmax > timestepmin:
-            time_days_upper = float(af.get_timestep_time(specfilename, timestepmax))
-            linelabel += f' to {time_days_upper:.2f}d'
-            print(f'Plotting {modelname} ({filename}) timesteps {timestepmin} to {timestepmax} (t={time_days_lower}d'
-                  f' to {time_days_upper}d)')
-        else:
-            print(f'Plotting {modelname} timestep {timestepmin} (t={time_days_lower}d)')
-
-        if from_packets:
-            # find any other packets files in the same directory
-            packetsfiles_thismodel = glob.glob(os.path.join(os.path.dirname(filename), 'packets**.out'))
-            print(packetsfiles_thismodel)
-            spectrum = af.get_spectrum_from_packets(packetsfiles_thismodel, time_days_lower, time_days_upper,
-                                                    lambda_min=args.xmin, lambda_max=args.xmax)
-        else:
-            # def filterfunc(arrayfnu):
-            #     from scipy.signal import savgol_filter
-            #     return savgol_filter(arrayfnu, 5, 2)
-
-            spectrum = af.get_spectrum(specfilename, timestepmin, timestepmax,)
-            #                          fnufilterfunc=filterfunc)
-
-        maxyvaluethisseries = spectrum.query(
-            '@args.xmin < lambda_angstroms and '
-            'lambda_angstroms < @args.xmax')['f_lambda'].max()
-
-        linestyle = ['-', '--'][int(index / 7) % 2]
-        spectrum['f_lambda_scaled'] = (spectrum['f_lambda'] / maxyvaluethisseries)
-        ycolumnname = 'f_lambda_scaled' if args.normalised else 'f_lambda'
-        spectrum.plot(x='lambda_angstroms', y=ycolumnname, ax=axis,
-                      linestyle=linestyle, lw=2.5 - (0.2 * index),
-                      label=linelabel, alpha=0.95, color=None)  # colorlist[index % len(colorlist)]
-        # dashes=dashesList[index], dash_capstyle=dash_capstyleList[index])
 
 
 if __name__ == "__main__":
