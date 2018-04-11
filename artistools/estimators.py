8c9f1185a8e2d68a23d875c816cef7f1b16856d8
Luke Shingles <luke.shingles@gmail.com>
2018-04-11 21:35:38 +0100

    Change line styles

--- artistools/estimators.py
+++ artistools/estimators.py
@@ -331,7 +331,7 @@ def plot_multi_ion_series(
 
         ylist.insert(0, ylist[0])
         dashes_list = [(4.5, 2, 3, 2, 4.5, 2), (5, 1), (2, 1), (6, 2), (6, 1)]
-        linestyle_list = ['-', '--', '-.', ':'] + [(0, x) for x in dashes_list]
+        linestyle_list = ['-.', '-', '--', ':'] + [(0, x) for x in dashes_list]
         linestyle = linestyle_list[ion_stage - 1]
         linewidth = [1.5, 1.5, 1.0, 1.0, 1.0][ion_stage - 1]
         # color = ['blue', 'green', 'red', 'cyan', 'purple', 'grey', 'brown', 'orange'][ion_stage - 1]
