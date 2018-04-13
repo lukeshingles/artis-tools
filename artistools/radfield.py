8fd2fd0b49225828399849843b245da22d696fa3
Luke Shingles <luke.shingles@gmail.com>
2018-04-13 16:27:45 +0100

    Reduce radfield legend size

--- artistools/radfield.py
+++ artistools/radfield.py
@@ -273,7 +273,7 @@ def plot_celltimestep(
     axis.set_ylim(ymin=0.0, ymax=ymax)
     axis.yaxis.set_major_formatter(at.ExponentLabelFormatter(axis.get_ylabel(), useMathText=True))
 
-    axis.legend(loc='best', handlelength=2, frameon=False, numpoints=1, prop={'size': 13})
+    axis.legend(loc='best', handlelength=2, frameon=False, numpoints=1, prop={'size': 11})
 
     print(f'Saving to {outputfile}')
     fig.savefig(str(outputfile), format='pdf')
