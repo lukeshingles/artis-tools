ca0f3f23cb537328e95120623e322ca84abf2224
Luke Shingles <luke.shingles@gmail.com>
2018-04-26 18:38:50 +0100

    Improve line styles

--- artistools/estimators.py
+++ artistools/estimators.py
@@ -375,8 +375,8 @@ def plot_multi_ion_series(
         plotlabel = f'{at.elsymbols[atomic_number]} {at.roman_numerals[ion_stage]}'
 
         ylist.insert(0, ylist[0])
-        dashes_list = [(4.5, 2, 3, 2, 4.5, 2), (5, 1), (2, 1), (6, 2), (6, 1)]
-        linestyle_list = ['-.', '-', '--', ':'] + [(0, x) for x in dashes_list]
+        dashes_list = [(5, 1), (2, 1), (6, 2), (6, 1)]
+        linestyle_list = ['-.', '-', '--', (0, (4, 1, 1, 1)), ':'] + [(0, x) for x in dashes_list]
         linestyle = linestyle_list[ion_stage - 1]
         linewidth = [1.5, 1.5, 1.0, 1.0, 1.0][ion_stage - 1]
         # color = ['blue', 'green', 'red', 'cyan', 'purple', 'grey', 'brown', 'orange'][ion_stage - 1]
