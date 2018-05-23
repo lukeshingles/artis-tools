1b2d87aeb8a738e0d3e01b46aef85cf23a74146b
Luke Shingles <luke.shingles@gmail.com>
2018-05-23 11:29:15 +0100

    Tweak spectral plot appear for paper

--- artistools/spectra.py
+++ artistools/spectra.py
@@ -23,17 +23,17 @@ refspectra = {
     '2010lp_20110928_fors2.txt':
         ('SN2010lp +264d (Taubenberger et al. 2013)', -1),
 
-    'sn2011fe_PTF11kly_20120822_norm.txt':
-        ('SN2011fe +364d (Mazzali et al. 2015)', 6.40),
+    'sn2011fe_PTF11kly_20120822_norm.txt':  # (Mazzali et al. 2015)
+        ('SN2011fe +364d', 6.40),
 
-    'dop_dered_SN2013aa_20140208_fc_final.txt':
-        ('SN2013aa +360d (Maguire et al. 2016)', 13.95),
+    'dop_dered_SN2013aa_20140208_fc_final.txt':  # (Maguire et al. 2016)
+        ('SN2013aa +360d', 13.95),
 
-    '2003du_20031213_3219_8822_00.txt':
-        ('SN2003du +221.3d (Stanishev et al. 2007)', 30.47),
+    '2003du_20031213_3219_8822_00.txt':  # (Stanishev et al. 2007)
+        ('SN2003du +221.3d', 30.47),
 
-    'FranssonJerkstrand2015_W7_330d_10Mpc.txt':
-        ('SUMO W7 (Fransson & Jerkstrand 2015) +330d', 10),  # Iwamoto+1999
+    'FranssonJerkstrand2015_W7_330d_10Mpc.txt':  # (Fransson & Jerkstrand 2015)
+        ('SUMO W7 +330d', 10),  # Iwamoto+1999
 
     'maurer2011_RTJ_W7_338d_1Mpc.txt':
         ('RTJ W7 Nomoto+1984 +338d (Maurer et al. 2011)', 1),
@@ -491,8 +491,64 @@ def make_spectrum_stat_plot(spectrum, figure_title, outputpath, args):
     plt.close()
 
 
+def get_time_range(timearray, timestep_range_str, timemin, timemax, timedays_range_str):
+    """Handle a time range specified in either days or timesteps."""
+    # assertions make sure time is specified either by timesteps or times in days, but not both!
+    timedays_is_specified = (timemin is not None and timemax is not None) or timedays_range_str is not None
+
+    if timemin and timemin > float(timearray[-1].strip('d')):
+        raise ValueError(f"timemin {timemin} is after the last timestep at {timearray[-1]}")
+    elif timemax and timemax < float(timearray[0].strip('d')):
+        raise ValueError(f"timemax {timemax} is before the first timestep at {timearray[0]}")
+
+    if timestep_range_str is not None:
+        if timedays_is_specified:
+            raise ValueError("Cannot specify both time in days and timestep numbers.")
+
+        if '-' in timestep_range_str:
+            timestepmin, timestepmax = [int(nts) for nts in timestep_range_str.split('-')]
+        else:
+            timestepmin = int(timestep_range_str)
+            timestepmax = timestepmin
+    elif timedays_is_specified:
+        if timedays_range_str is not None:
+            if '-' in timedays_range_str:
+                timemin, timemax = [float(timedays) for timedays in timedays_range_str.split('-')]
+            else:
+                timeavg = float(timedays_range_str)
+                timedelta = 10
+                timemin, timemax = timeavg - timedelta, timeavg + timedelta
+
+        timestepmin = None
+        for timestep, time in enumerate(timearray):
+            timefloat = float(time.strip('d'))
+            if float(timemin) <= timefloat:
+                timestepmin = timestep
+                break
+
+        if timestepmin is None:
+            print(f"Time min {timemin} is greater than all timesteps ({timearray[0]} to {timearray[-1]})")
+            sys.exit()
+
+        if not timemax:
+            timemax = float(timearray[-1].strip('d'))
+        for timestep, time in enumerate(timearray):
+            timefloat = float(time.strip('d'))
+            if timefloat + at.get_timestep_time_delta(timestep, timearray) <= timemax:
+                timestepmax = timestep
+        if timestepmax < timestepmin:
+            raise ValueError("Specified time range does not include any full timesteps.")
+    else:
+        raise ValueError("Either time or timesteps must be specified.")
+
+    time_days_lower = float(timearray[timestepmin])
+    time_days_upper = float(timearray[timestepmax]) + at.get_timestep_time_delta(timestepmax, timearray)
+
+    return timestepmin, timestepmax, time_days_lower, time_days_upper
+
+
 def plot_artis_spectrum(axes, modelpath, args, scale_to_peak=None, from_packets=False, filterfunc=None, **plotkwargs):
-    (timestepmin, timestepmax, args.timemin, args.timemax) = at.get_time_range(
+    (timestepmin, timestepmax, args.timemin, args.timemax) = get_time_range(
         at.get_timestep_times(modelpath), args.timestep, args.timemin, args.timemax, args.timedays)
 
     modelname = at.get_model_name(modelpath)
@@ -554,7 +610,7 @@ def make_spectrum_plot(modelpaths, axes, filterfunc, args, scale_to_peak=None):
         plotkwargs['linestyle'] = '--' if (int(index / 7) % 2) else '-'
         plotkwargs['linewidth'] = 1.5  # - (0.1 * index)
         if index < 3:
-            plotkwargs['color'] = ['orange', 'red', 'blue'][index]
+            plotkwargs['color'] = ['blue', 'orange', 'red'][index]
         # if index < 10:
         #     plotkwargs['color'] = f'C{index}'
         plot_artis_spectrum(axes, modelpath, args=args, scale_to_peak=scale_to_peak, from_packets=args.frompackets,
@@ -575,7 +631,7 @@ def make_emissionabsorption_plot(modelpath, axis, filterfunc, args, scale_to_pea
     arraylambda_angstroms = const.c.to('angstrom/s').value / arraynu
 
     (timestepmin, timestepmax,
-     args.timemin, args.timemax) = at.get_time_range(
+     args.timemin, args.timemax) = get_time_range(
          timearray, args.timestep, args.timemin, args.timemax, args.timedays)
 
     modelname = at.get_model_name(modelpath)
@@ -780,7 +836,7 @@ def write_flambda_spectra(modelpath, args):
     if not args.timestep:
         args.timestep = f'0-{number_of_timesteps - 1}'
 
-    (timestepmin, timestepmax, args.timemin, args.timemax) = at.get_time_range(
+    (timestepmin, timestepmax, args.timemin, args.timemax) = get_time_range(
         timearray, args.timestep, args.timemin, args.timemax, args.timedays)
 
     with open(outdirectory / 'spectra_list.txt', 'w+') as spectra_list:
