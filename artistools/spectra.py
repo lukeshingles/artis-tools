350607c8366bc309e93e06c975d5828ff7508449
ccollins <c_collins94@hotmail.co.uk>
2020-02-10 14:33:14 +0000

    Print list of included ions in format of -fixedionlist

--- artistools/spectra.py
+++ artistools/spectra.py
@@ -866,6 +866,7 @@ def sort_and_reduce_flux_contribution_list(
     remainder_fluxcontrib = 0
 
     contribution_list_out = []
+    plotted_ion_list = []
     numotherprinted = 0
     entered_other = False
     if greyscale:
@@ -899,6 +900,7 @@ def sort_and_reduce_flux_contribution_list(
         if numotherprinted < 20:
             integemiss = abs(np.trapz(row.array_flambda_emission, x=arraylambda_angstroms))
             integabsorp = abs(np.trapz(-row.array_flambda_absorption, x=arraylambda_angstroms))
+            plotted_ion_list.append(row.linelabel)
             if integabsorp > 0. and integemiss > 0.:
                 print(f'{row.fluxcontrib:.1e}, emission {integemiss:.1e}, '
                       f"absorption {integabsorp:.1e} [erg/s/cm^2]: '{row.linelabel}'")
@@ -909,6 +911,8 @@ def sort_and_reduce_flux_contribution_list(
 
             if entered_other:
                 numotherprinted += 1
+
+    print("List of ions included:", *plotted_ion_list[:14], sep="' '", end="'\n", )
 
     if remainder_fluxcontrib > 0. and not hideother:
         contribution_list_out.append(fluxcontributiontuple(
