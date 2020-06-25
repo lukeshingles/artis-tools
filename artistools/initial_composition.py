1b71861b9dc84b6c340dc02d8475d3c52289ecba
ccollins <c_collins94@hotmail.co.uk>
2020-06-25 10:54:20 +0100

    Use rasterized to reduce file size of plot

--- artistools/initial_composition.py
+++ artistools/initial_composition.py
@@ -78,7 +78,7 @@ def plot_3d_initial_abundances(modelpath, args):
     y = plotvals[f'cellpos_in[{plotaxis2}]'] / t_model * (u.cm/u.day).to('km/s') / 10 ** 3
     # fig = plt.figure(figsize=(5, 5))
     ax = plt.subplot(111)
-    im = ax.scatter(x, y, c=plotvals[ion], marker="8")
+    im = ax.scatter(x, y, c=plotvals[ion], marker="8", rasterized=True)  # cmap=plt.get_cmap('PuOr')
 
     cbar = plt.colorbar(im)
     # cbar.set_label(label=ion, size='x-large') #, fontweight='bold')
