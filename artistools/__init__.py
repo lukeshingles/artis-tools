88c159874d1b78a5f358f6192c89b7e9d0d9150d
Luke Shingles <luke.shingles@gmail.com>
2020-11-18 10:41:28 +0000

    Update __init__.py

--- artistools/__init__.py
+++ artistools/__init__.py
@@ -93,7 +93,7 @@ def diskcache(ignoreargs=[], ignorekwargs=[], saveonly=False, quiet=False, saveg
         @wraps(func)
         def wrapper(*args, **kwargs):
             cachefolder = Path('__artistoolscache__.nosync')
-            if cachefolder.is_dir()::
+            if cachefolder.is_dir():
                 xattr.setxattr(cachefolder, "com.dropbox.ignored", b'1')
 
             namearghash = hashlib.sha1()
